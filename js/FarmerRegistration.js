31870cd91d8425c0dd1f79aa3e5669ba5023e331
SalamBhai <saulasheriffdeen@gmail.com>
2022-11-30 11:55:33 +0100

    Changes for the current branch

--- js/FarmerRegistration.js
+++ js/FarmerRegistration.js
@@ -1,42 +1,52 @@
 let city = document.querySelector("#city");
-let country = document.querySelector("#country");
-fetch('https://localhost:7133/api/GatewaysTest/GetCountryAndCitiesAsync', {
-                    method : "GET",
-         }).then((response) => response.json()).then((result) => {
-                    console.log(result)
-                    result.forEach(x => {
-                        country.innerHTML += `
-                        <option value="${x.country}">${x.country}</option>
-                        `
-                    })
-         }).catch((e) => 
-         {
-                    console.log(e);
-         })
 
-city.addEventListener("click", (e) =>{
-    fetch('https://localhost:7133/api/GatewaysTest/GetCountryAndCitiesAsync', {
+let country = document.querySelector("#country");
+let countriesAndCities;
+const baseUrl = `https://localhost:7133/api/`
+let farmLocations = [];
+
+consumeCountriesAndCities();
+function consumeCountriesAndCities()
+{
+    fetch(`${baseUrl}GatewaysTest/GetCountryAndCitiesAsync`, {
         method : "GET",
-        }).then((response) => response.json()).then((result) => {
-                console.log(result)
-                result.forEach(x => {
-                    if(x.country == country.value)
-                    {
-                        console.log("25", x.cities);
-                        x.cities.forEach(y => {
-                            city.innerHTML += `
-                            <option value="${y}">${y}</option>
-                            `
-                        })
-                        
-                    }
-                    
-                })
-        }).catch((e) => 
-        {
-                console.log(e);
-        })
+    }).then((response) => response.json()).then((result) => {
+        countriesAndCities = result;
+    result.forEach(x => 
+    {
+
+        country.innerHTML += `
+        <option value="${x.country}">${x.country}</option>
+        `
+    })
 })
+.catch((e) => 
+{
+ console.error(e);
+})
+}
+
+ function setCities(e)
+{
+  
+ countriesAndCities.forEach( x => {
+    if(x.country === country.value)
+    {
+       farmLocations = x.cities;
+       console.log(farmLocations);
+        x.cities.forEach(y => {
+        city.innerHTML += `
+            <option class="options" value="${y}" selected=false>${y}</option>`});
+        console.log(e);
+    }})
+  //let cities = document.querySelector(".options");
+  //printCities(cities)
+//   cities.addEventListener("mouseover",() =>{
+//     console.log(city.options);
+// })
+}
+
+
 
 let submitButton = document.querySelector("#sub-btn");
 let listButton = document.querySelector("#list-btn");
@@ -47,7 +57,8 @@ submitButton.addEventListener("click", (e) => {
 
  e.preventDefault();
           let nam = document.querySelector("#name");
-          let country = document.querySelector("#country");
+        //   let country = document.querySelector("#country");
+          
           let phoneNumbe = document.querySelector("#Number");
           let languag = document.querySelector("#language");
           Data = {
@@ -56,42 +67,54 @@ submitButton.addEventListener("click", (e) => {
                     location : city.value,
                     language : languag.value,
           };
-          
+          console.log(Data.location,"70");
          fetch('https://localhost:7133/api/Farmer/RegisterFarmerAsync', {
                     method : "POST",
                     headers : {
-                              'Content-Type': 'application/json'
+                         'Content-Type': 'application/json'
                     },
                     body : JSON.stringify(Data)
          }).then((response) => response.json()).then((result) => {
+                  if(result.data === null) 
+                  {
+                    alert(`Farmer with phone number:${phoneNumbe.value} already exists`)
+                  }
                     console.log(result)
                     localStorage.setItem("farmerId", result.data.id);
+                    localStorage.setItem("country",country.value);
                     alert(result.messages);
                     farmReg.style.display = "block";
                     farmerReg.style.display = "none";
          }).catch((e) => 
          {
                     alert(e.message);
-                    console.log(e);
+                    console.error(e);
          })
 })
 
-
+let location2 = document.querySelector("#locatedCity");
 let  farms = [];
+location2.addEventListener("click",()=>{
+    farmLocations.forEach(loca =>{
+        location2.innerHTML += `
+        <option class="options" value="${loca}" selected=false>${loca}</option>`});
+    
+})
 
 listButton.addEventListener("click", (e) => {
 
  e.preventDefault();
           let name = document.querySelector("#farmname");
-          let location = document.querySelector("#locatedCity");
           let crop = document.querySelector("#crop");
           let tableBody = document.querySelector("#table-body");
+        
           Data = {
                     farmName : name.value,
                     CropTypeId : crop.value,
-                    locatedCity : location.value,
+                    locatedCity : location2.value,
                     farmerId: localStorage.getItem("farmerId")
           }
+          console.log(locatedCity,117);
           farms.push(Data);
           console.log(farms);
           tableBody.innerHTML = "";
@@ -120,6 +143,7 @@ console.log("seen");
                     body : JSON.stringify(farms)
          }).then((response) => response.json()).then((result) => {
                     console.log(result)
+                    alert(result.messages[2]);
          }).catch((e) => 
          {
                     console.log(e);
--- js/WeatherSearch.js
+++ js/WeatherSearch.js
@@ -13,7 +13,40 @@ let background = "";
 let night = "/assets/night.png";
 let sunrise = "/assets/Sunrise.png";
 let day = "/assets/Day.png";
+let cities = [];
+let xCities;
+let countriesAndCities;
+const baseUrl = `https://localhost:7133/api/`
+fetch(`${baseUrl}GatewaysTest/GetCountryAndCitiesAsync`, {
+ method : "GET",
+}).then((response) => response.json()).then((result) => {
+    countriesAndCities = result;
+    result.forEach(x => 
+    {
+       x.cities.forEach( x=> 
+      {
+        cities.push(x);
+      })})
+            console.log(cities);
+      }).catch((e) => {
+            console.log(e);
+         })
 
+
+
+         
+// function fetchdrop()
+// {
+//   const choices = new Choices(".choices-select", {
+//     silent: true,
+//     choices: cities.map(city2 => ({
+//       value: city2,
+//       label: city2,
+//     })),
+//     removeItemButton: true,
+//   });
+//   console.log(choices.choices)
+// }
 //RUN THE DEFAULT FORECAST ONCE THE PAGE LOADS
 window.onload = getWeather();
 
@@ -24,7 +57,15 @@ getBtn.addEventListener("click", function () {
 
 //EVENT LISTENER FOR PRESSING ENTER
 input.addEventListener("keyup", function (e) {
+ 
   if (e.key === "Enter") {
+    if(!(cities.includes(input.value)) || input.value.includes("-"))
+    {
+      alert("Please Input A Valid African City ");
+      return;
+      // location.reload();
+    }
+   
     getWeather();
   }
 });
@@ -35,40 +76,26 @@ function getWeather() {
   title.innerHTML = "";
   //SET THE LOCATION FROM THE USER INPUT
   input.value === "" ? (loc = "Abeokuta") : (loc = input.value);
-  console.log(url);
+  console.log(cities);
+  
   fetch(url + loc)
+
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       //SET THE TITLE WITH THE NAME OF THE LOCATION
       console.log(data.data[0]);
-      title.innerHTML += `<h1>24hr Weather forecast for ${data.data[0].city.name}</h1>`;
+      title.innerHTML += `<h1 style="color:white;font-weight:light; background:transparent;">24hr Weather forecast for ${data.data[0].city.name}</h1>`;
       const dataSetAccessed = data.data[0].dailyForecasts;
      for (let i = 0; i < data.data[0].dailyForecasts.length; i++) {
 
-     // SET THE BACKGROUND DEPENDING ON THE TIME OF DAY
-          date = data.data[0].dailyForecasts;
-          console.log(date);
-          text = String(dataSetAccessed[i].dt);
-          console.log(text);
-         time = String(dataSetAccessed[i].dt);
-         console.log(time.length);
-       if (Number(time) > 19) {
-         background = night;
-        }
-        else if (Number(time) > 5 && Number(time) < 9) 
-         {
-          background = sunrise;
-        } 
-       else if (Number(time) > 9 && Number(time) < 19) {
-          background = day;
-        }
+    
         console.log(`<img src="${icons + dataSetAccessed[i].weather[0].icon}@2x.png"/>`)
     //     //RENDER THE API DATA
      var newDate = (dataSetAccessed[i].dates.forecastDate).split("T")[0];
      console.log(newDate);
       main.innerHTML += `
-       <div class="update" style="background: url(${background}); background-size: cover">
+       <div class="update" style="background: url(${night}); background-size: cover">
 
         <h2>${dataSetAccessed[i].weather[0].main}</h2>
        <div class="icon" style="border: 5px solid #10A1E5">
@@ -87,4 +114,4 @@ function getWeather() {
     });
 }
 
-//url + 'London&appid=4bd375ae392fd932766eeb5bd0b6633e'
+
